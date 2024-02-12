1b0d6e06c0b23bf3fb548b4066a8a3b77bedea13
Renato <renatimnerdin@gmail.com>
2024-02-12 10:49:39 -0300

    math

---
+++ 4-estruturas/7_math/script.js
@@ -0,0 +1,8 @@
+let maior = Math.max(6,12,28,55,8)
+console.log(maior)
+let menor = Math.min(6,12,28,55,8);
+console.log(menor)
+let arrendodar = Math.round(5.57)
+console.log(arrendodar)
+let arrendodarParaCima = Math.ceil(6.29)
+console.log(arrendodarParaCima)

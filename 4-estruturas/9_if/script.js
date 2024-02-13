d5d7226a66af28359044d34feb8cc4e78a4418bc
Renato <renatimnerdin@gmail.com>
2024-02-13 09:49:24 -0300

    if

---
+++ 4-estruturas/9_if/script.js
@@ -0,0 +1,21 @@
+let idade = 15
+
+if (idade == 19) {
+    console.log("A idade é = 19")
+}
+
+if(idade > 25) {
+    console.log("A idade é maior que 25")
+}
+
+let nome = "Renato"
+
+if(nome == "Renato"  && idade > 10) {
+    console.log("Liberado!")
+}
+
+let passaporte = true
+
+if(nome == "Renato" && idade > 30 || passaporte == true) {
+    console.log("Liberado!")
+}

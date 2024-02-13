dc861408b1bfecc19cfb9ad3a528fa9dc350de48
Renato <renatimnerdin@gmail.com>
2024-02-13 10:16:12 -0300

    else if

---
+++ 4-estruturas/11_else_if/script.js
@@ -0,0 +1,15 @@
+let nome = "Renato"
+
+if(nome != undefined && nome == "Joaquim") {
+    console.log("Nome está definido")
+} else if(nome == "Renato"  && nome.length > 5) {
+    console.log("O nome é Renato")
+} else {
+    console.log("O nome não é Renato")
+}
+
+if (1 > 2) {
+    console.log("Teste")
+} else if( 1 == 1) {
+    console.log("Testando")
+}

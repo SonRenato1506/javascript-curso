51f0ac99bc7d63039a1bd4b69f0424c33d9656e1
Renato <renatimnerdin@gmail.com>
2024-02-14 10:24:54 -0300

    switch

---
+++ 4-estruturas/19_switch/script.js
@@ -0,0 +1,13 @@
+nome = "Arthur"
+
+switch(nome) {
+    case "Renato":
+        console.log("O nome é Renato")
+        break;
+    case "Arthur":
+        console.log("O nome é Arthur")
+        break;
+    default:
+        console.log("O nome não foi encontrado")
+        break;
+}

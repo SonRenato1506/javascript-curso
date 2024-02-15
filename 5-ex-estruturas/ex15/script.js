4613ffe06ae1aee18517cd0607ff156283c75a3f
Renato <renatimnerdin@gmail.com>
2024-02-15 08:56:41 -0300

    ex15

---
+++ 5-ex-estruturas/ex15/script.js
@@ -0,0 +1,13 @@
+n = 6
+s = 0
+for(c = n-1; c > 1; c--) {
+    if(n % c == 0) {
+        s++
+    }
+}
+
+if(s == 0) {
+    console.log(`${n} é primo`)
+} else {
+    console.log(`${n} não é primo`)
+}

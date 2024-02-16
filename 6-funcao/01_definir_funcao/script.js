0756c933a996b3d2b4da51d3b29d373171bb3546
Renato <renatimnerdin@gmail.com>
2024-02-16 09:21:36 -0300

    funcao

---
+++ 6-funcao/01_definir_funcao/script.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole() {
+    console.log("Olá Mundo!")
+} 
+
+imprimirNoConsole()
+
+function imprimirNumero(num) {
+    console.log("O número é: " + num)
+}
+
+imprimirNumero(2)
+imprimirNumero(4)
+imprimirNumero(6)
+imprimirNumero(8)
+
+const numeroAleatorio = function() {
+    console.log(Math.random())
+}
+
+numeroAleatorio()
+numeroAleatorio()

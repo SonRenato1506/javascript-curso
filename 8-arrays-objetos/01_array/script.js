40f53dee5931ab61303c963935f5adcaf9948246
Renato <renatimnerdin@gmail.com>
2024-02-29 08:44:31 -0300

    Array

---
+++ 8-arrays-objetos/01_array/script.js
@@ -0,0 +1,10 @@
+let arr = [1, 4, 5, 6, 7]
+let nomes = ['Matheus', "João","Aléxia"]
+let bool = [true, false]
+
+let misturado = [1, "Renato", true]
+
+console.log(arr)
+console.log(nomes)
+console.log(bool)
+console.log(misturado)

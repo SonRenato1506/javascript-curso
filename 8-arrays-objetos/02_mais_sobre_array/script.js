de11464bf47ae6b7318b50a09674840ae858df34
Renato <renatimnerdin@gmail.com>
2024-02-29 09:05:14 -0300

    Update script.js

--- 8-arrays-objetos/02_mais_sobre_array/script.js
+++ 8-arrays-objetos/02_mais_sobre_array/script.js
@@ -2,7 +2,7 @@ let arr = [1, 4, 5, 6, 7]
 let nomes = ['Matheus', "João","Aléxia"]
 let bool = [true, false]
 
-console.log(arr[1])
+console.log(arr[2])
 console.log(nomes[0])
-console.log(bool[2])
+console.log(bool[1])
 console.log(arr[arr.length-1])

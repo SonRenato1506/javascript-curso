f06bb1e531741a471f508566fdfce73dc0b265ff
Renato <renatimnerdin@gmail.com>
2024-03-05 09:30:20 -0300

    propriedades de um objeto

---
+++ 8-arrays-objetos/06_objetos_criar_prop/script.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    nome: "Renato",
+    idade: 25,
+    profissao: "Programador",
+}
+
+console.log(pessoa.nome)
+
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)

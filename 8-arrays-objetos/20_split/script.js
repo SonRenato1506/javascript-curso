90ca89ad626790dd57482adee9ee4af46cce2175
Renato <renatimnerdin@gmail.com>
2024-03-29 09:32:29 -0300

    09:32

---
+++ 8-arrays-objetos/20_split/script.js
@@ -0,0 +1,8 @@
+let frase = "O rato roueu a roupa do rei de roma"
+console.log(frase)
+
+palavra = frase.split(" ")
+
+for(c = 0; c < palavra.length; c++) {
+    console.log(palavra[c])
+}
---
+++ 8-arrays-objetos/21_join/script.js
@@ -0,0 +1,9 @@
+let frase = "O rato roueu a roupa do rei de roma"
+console.log(frase)
+
+palavra = frase.split(" ")
+
+console.log(palavra)
+
+let fraseMontada = palavra.join('-')
+console.log(fraseMontada)

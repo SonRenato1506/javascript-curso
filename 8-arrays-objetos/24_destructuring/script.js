cf9668df1c5f5c81a5d2e50b315ecf49d3d7cba3
Renato <renatimnerdin@gmail.com>
2024-04-01 09:13:18 -0300

    destructuring

---
+++ 8-arrays-objetos/24_destructuring/script.js
@@ -0,0 +1,9 @@
+const person ={
+    name: "Jhon",
+    lastname: "Doe"
+}
+
+const {name: fname,lastname: lname} = person;
+
+console.log(fname)
+console.log(lname)
---
+++ 8-arrays-objetos/25_destructuring_array/script.js
@@ -0,0 +1,7 @@
+let nomes = ["Renato",  "Arthur", "Márcio"]
+
+let [nomeA, nomeB, nomeC] = nomes;
+
+console.log(nomeA)
+console.log(nomeB)
+console.log(nomeC)

12e4b03a91b82da8ba19b4e381f7bbd36253101a
Renato <renatimnerdin@gmail.com>
2024-04-16 09:10:28 -0300

    tratamento input

---
+++ 12-debugs-erros/04_tratamento_input/script.js
@@ -0,0 +1,12 @@
+function checarNumero (num)  {
+    let number = Number(num)
+    if(Number.isNaN(number)) {
+    alert("Por favor, passe só números para o programa")
+    } else {
+        return number
+    }
+}
+
+let number = prompt("Digite um número");
+
+checarNumero(number)

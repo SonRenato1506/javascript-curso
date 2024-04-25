166070649fcd3b6e7a406cb17d79be821bae937f
Renato <renatimnerdin@gmail.com>
2024-04-25 10:50:06 -0300

    10:50

---
+++ 15-programação-assincrona/02_Promisses/script.js
@@ -0,0 +1,14 @@
+// let promessa = Promise.resolve(4 * 8)
+
+// console.log("Algum codigo")
+
+// promessa.then((value) => console.log(`A soma é ${value}`))
+
+let p = Promise.resolve(5)
+
+console.log("Outros códigos")
+
+console.log(p)
+
+p.then((value) => {return value + 5})
+.then((value) => {console.log(value)})
